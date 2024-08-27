2ec49c5492c05c1867acdd39ac4fdc35aad66466
Madame-Aehm <emilyrachelstickler@gmail.com>
2024-08-27 15:51:50 +0200

    correct typo

--- client/next.config.js
+++ client/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'https://squad-builder-server.vercel.app',
+        hostname: 'squad-builder-server.vercel.app',
       },
       {
         // protocol: 'https',
